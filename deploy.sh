a6319dc3fd56f37312f8a538bfd07a1d3a781705
BurhanCantCode <burhanuddinkhatri@gmail.com>
2025-01-23 23:25:20 +0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 # ----------------------
 # KUDU Deployment Script
+# Version: 1.0.17
 # ----------------------
 
 # Helpers
@@ -14,13 +15,6 @@ exitWithMessageOnError () {
     exit 1
   fi
 }
-
-# Prerequisites
-# ------------
-
-# Verify node.js installed
-hash node 2>/dev/null
-exitWithMessageOnError "Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment."
 
 # Setup
 # -----
@@ -55,9 +49,14 @@ if [[ ! -n "$KUDU_SYNC_CMD" ]]; then
   exitWithMessageOnError "npm failed"
 
   if [[ ! -n "$KUDU_SERVICE" ]]; then
-    # In case we are running locally, need to set these
-    KUDU_SYNC_CMD=kudusync
+    # In case we are running locally, need to handle Node.js installation
+    if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
+      eval $NPM_CMD install
+      exitWithMessageOnError "npm failed"
+    fi
   fi
+
+  KUDU_SYNC_CMD=$(npm bin)/kudusync
 fi
 
 # Node Helpers
@@ -73,7 +72,7 @@ selectNodeVersion () {
       NODE_EXE=`cat "$DEPLOYMENT_TEMP/__nodeVersion.tmp"`
       exitWithMessageOnError "getting node version failed"
     fi
-
+    
     if [[ -e "$DEPLOYMENT_TEMP/__npmVersion.tmp" ]]; then
       NPM_JS_PATH=`cat "$DEPLOYMENT_TEMP/__npmVersion.tmp"`
       exitWithMessageOnError "getting npm version failed"
@@ -99,27 +98,33 @@ echo Handling node.js deployment.
 # 1. Select node version
 selectNodeVersion
 
-# 2. Install npm packages for client
-if [ -e "$DEPLOYMENT_SOURCE/client/package.json" ]; then
-  cd "$DEPLOYMENT_SOURCE/client"
+# 2. Install npm packages
+if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
+  cd "$DEPLOYMENT_SOURCE"
+  echo "Running npm install"
   eval $NPM_CMD install
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
 
-# 3. Install npm packages for server
-if [ -e "$DEPLOYMENT_SOURCE/server/package.json" ]; then
-  cd "$DEPLOYMENT_SOURCE/server"
-  eval $NPM_CMD install
-  exitWithMessageOnError "npm failed"
-  cd - > /dev/null
-fi
-
-# 4. Build client
+# 3. Install client packages and build
 if [ -e "$DEPLOYMENT_SOURCE/client/package.json" ]; then
   cd "$DEPLOYMENT_SOURCE/client"
+  echo "Running npm install in client"
+  eval $NPM_CMD install
+  exitWithMessageOnError "client npm failed"
+  echo "Building client"
   eval $NPM_CMD run build
   exitWithMessageOnError "client build failed"
+  cd - > /dev/null
+fi
+
+# 4. Install server packages
+if [ -e "$DEPLOYMENT_SOURCE/server/package.json" ]; then
+  cd "$DEPLOYMENT_SOURCE/server"
+  echo "Running npm install in server"
+  eval $NPM_CMD install
+  exitWithMessageOnError "server npm failed"
   cd - > /dev/null
 fi
 
